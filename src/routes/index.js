d67bcae49c90343589745e6416a6c50a80f2aa69
Rishabh Mittal <rishabhmittal2407@gmail.com>
2025-08-24 17:30:47 +0530

    Update: Error Handling

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ const v2Routes = require('./v2');
 
 const router = express.Router();
 
+
 router.use('/v1', v1Routes);
 router.use('/v2', v2Routes);
 
---
+++ src/utils/common/success-response.js
@@ -0,0 +1,8 @@
+const success = {
+    success: true,
+    message: 'Successfully completed the action',
+    data: {},
+    error: {}
+}
+
+module.exports = success;
